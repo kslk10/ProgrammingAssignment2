59c741cad1e7feb24fbd785bbf468f66fdf46cf4
Sumanth <sumanth.kslk10@gmail.com>
2014-08-21 14:01:29 +0530

    Description of the file

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Put comments here that give an overall description of what your
+## This script give the cached inverse of matrix
 ## functions do
 
 ## Write a short comment describing this function
